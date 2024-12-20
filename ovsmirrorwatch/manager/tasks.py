e1d21f54d2944d62cfb9878edf5ae72a69c8dc11
dmerkouris <d.merkouris@ppcgroup.com>
2024-12-20 23:33:50 +0200

    Updated mirror monitor capabilities

--- ovsmirrorwatch/manager/tasks.py
+++ ovsmirrorwatch/manager/tasks.py
@@ -3,8 +3,7 @@ from celery.utils.log import get_task_logger
 from api_ovs.OvsApi import OVSAPI
 from manager.models import OVSManager
 from mirror.models import OVSMirror
-from api_ovs.ovs_mirror_monitor_v3 import check_and_repair_mirrors
-
+import functools
 
 logger = get_task_logger(__name__)
 
@@ -16,27 +15,65 @@ def check_ovsdb_manager(server_name):
 
     # Get all mirrors from the live system
     live_mirrors = OVSDBManager.get_mirrors_overview()
-    
-    # check_and_repair_mirrors(OVSDBManager, OVSManager.objects.all(), live_mirrors)
+    live_named_mirrors = {live_mirror.get('name'):live_mirror for live_mirror in live_mirrors} #helper dict
+
+    db_mirrors = OVSManager.objects.all()
 
     # For each mirror of the Django DB
-    for db_mirror in OVSMirror.objects.all():
+    for db_mirror in db_mirrors:
 
-        # Check if the mirror exists in the real system
-        live_mirror = [item for item in live_mirrors if item.get('name')==db_mirror.name]
-        if len(live_mirror) > 0:
+        mirror_name = db_mirror.name
+        bridge_name = db_mirror.out_port.bridge
+        select_src_ports = db_mirror.src_ports
+        select_dst_ports = db_mirror.dst_ports
+        output_port = db_mirror.out_port
 
-            live_mirror = live_mirror[0]
+        if mirror_name in live_named_mirrors.keys():
+            # Update health status
+            db_mirror.health = True
+            logger.info(f"Mirror {db_mirror.name} is live.")
 
-            # If exists, check if the components of the live mirror correspond to the db_mirror components
-            # live_src_ports = live_mirror["select_src_port"]
-            # live_dst_ports = live_mirror["select_dst_port"]
-            # live_output_port = live_mirror["output_port"]
-            # if not, destroy and create again the mirror, based on the db_mirror components
+            # Check for misconfiguration. If yes change the mirror config to the desired on based on the db mirror.
+            live_mirror = live_named_mirrors[db_mirror.name]
 
+            # Check config of live and db mirror setups
+            if any(functools.reduce(lambda x, y : x and y, map(lambda p, q: p == q,select_src_ports,live_mirror["select_src_port"]), False),
+                   functools.reduce(lambda x, y : x and y, map(lambda p, q: p == q,select_dst_ports,live_mirror["select_dst_port"]), False),
+                   functools.reduce(lambda x, y : x and y, map(lambda p, q: p == q,output_port,live_mirror["output_port"]), False)):
+                
+                db_mirror.health = False
+                logger.info(f"Mirror {db_mirror.name} is live but misconfigured.")
+
+
+                # Destroy old mirror and set up a new one based on the db mirror 
+                if OVSAPI.destroy_mirror(mirror_name, bridge_name):
+                    logger.info(f"Destroyed mirror {db_mirror.name}.")
+                    
+                    # Reset the mirror on OVS Manager
+                    if OVSAPI.create_mirror(mirror_name, bridge_name, select_src_ports, select_dst_ports, output_port):
+                        logger.info(f"Reseted mirror {db_mirror.name} successfully.")
+                        # Update health status
+                        db_mirror.health = True
+                    else:
+                        # Update health status
+                        db_mirror.health = False
+                        logger.info(f"Failed to reset mirror {db_mirror.name}")
+                        
+                else:
+                    logger.info(f'Failed to destroy mirror {db_mirror.name}')
+                
+                
+
+        # Check if mirror does not exist. If not recreate the mirror
         else:
-            # if the mirror does not exist, create it, based on the db_mirror components
-            logger.info("The mirror " + db_mirror.name + " does not exist.")
+            # Update health status
+            db_mirror.health = False
+            
+            #Recreate the mirror on the Mirror Manager
+            OVSAPI.create_mirror(mirror_name, bridge_name, select_src_ports, select_dst_ports, output_port)
+        
+        
+    
 
                 
     logger.info("The sample task just ran for " + server_name)
