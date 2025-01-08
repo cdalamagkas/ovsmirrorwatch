b95c738046af1f882b92f581971300504e5997e4
dmerkouris <d.merkouris@ppcgroup.com>
2025-01-09 01:34:42 +0200

    Debug progressed

--- ovsmirrorwatch/manager/tasks.py
+++ ovsmirrorwatch/manager/tasks.py
@@ -3,7 +3,7 @@ from celery.utils.log import get_task_logger
 from api_ovs.OvsApi import OVSAPI
 from manager.models import OVSManager
 from mirror.models import OVSMirror
-import functools
+
 
 logger = get_task_logger(__name__)
 
@@ -23,34 +23,35 @@ def check_ovsdb_manager(server_name):
     for db_mirror in db_mirrors:
 
         mirror_name = db_mirror.name
-        bridge_name = db_mirror.out_port.bridge
-        select_src_ports = db_mirror.src_ports
-        select_dst_ports = db_mirror.dst_ports
-        output_port = db_mirror.out_port
+        bridge_name = db_mirror.out_port.bridge.ovs_name
+        select_src_ports = [f_name["friendly_name"] for f_name in db_mirror.src_ports.values('friendly_name')]
+        select_dst_ports = [f_name["friendly_name"] for f_name in db_mirror.dst_ports.values('friendly_name')]
+        output_port = db_mirror.out_port.ovs_name
+
+
 
         if mirror_name in live_named_mirrors.keys():
+            live_mirror = live_named_mirrors[mirror_name]
             # Update health status
             db_mirror.health = True
             logger.info(f"Mirror {db_mirror.name} is live.")
 
             # Check for misconfiguration. If yes change the mirror config to the desired on based on the db mirror.
-            live_mirror = live_named_mirrors[db_mirror.name]
-
             # Check config of live and db mirror setups
-            if any(functools.reduce(lambda x, y : x and y, map(lambda p, q: p == q,select_src_ports,live_mirror["select_src_port"]), False),
-                   functools.reduce(lambda x, y : x and y, map(lambda p, q: p == q,select_dst_ports,live_mirror["select_dst_port"]), False),
-                   functools.reduce(lambda x, y : x and y, map(lambda p, q: p == q,output_port,live_mirror["output_port"]), False)):
+            if any([select_src_ports == live_mirror["select_src_port"],
+                        select_dst_ports == live_mirror["select_dst_port"],
+                        output_port == live_mirror["output_port"]]):
                 
                 db_mirror.health = False
                 logger.info(f"Mirror {db_mirror.name} is live but misconfigured.")
 
 
-                # Destroy old mirror and set up a new one based on the db mirror 
-                if OVSAPI.destroy_mirror(mirror_name, bridge_name):
+                # Destroy old mirror and set up a new one based on the db mirror
+                if OVSDBManager.destroy_mirror(mirror_name, bridge_name):
                     logger.info(f"Destroyed mirror {db_mirror.name}.")
                     
                     # Reset the mirror on OVS Manager
-                    if OVSAPI.create_mirror(mirror_name, bridge_name, select_src_ports, select_dst_ports, output_port):
+                    if OVSDBManager.create_mirror(mirror_name, bridge_name, select_src_ports, select_dst_ports, output_port):
                         logger.info(f"Reseted mirror {db_mirror.name} successfully.")
                         # Update health status
                         db_mirror.health = True
@@ -68,9 +69,7 @@ def check_ovsdb_manager(server_name):
         else:
             # Update health status
             db_mirror.health = False
-
-            #Recreate the mirror on the Mirror Manager
-            OVSAPI.create_mirror(mirror_name, bridge_name, select_src_ports, select_dst_ports, output_port)
+            OVSDBManager.create_mirror(mirror_name, bridge_name, select_src_ports, select_dst_ports, output_port)
 
                 
     logger.info("The sample task just ran for " + server_name)
